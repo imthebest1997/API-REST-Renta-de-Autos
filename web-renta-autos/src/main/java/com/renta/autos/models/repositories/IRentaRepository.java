1e3ad2548e2f1f8357d37f76f1b48e1b8f8c07df
Alejandro Israel Gonzalez <imthebest_1997@hotmail.com>
2021-08-02 16:21:59 -0500

    CRUD operations

--- web-renta-autos/src/main/java/com/renta/autos/models/repositories/IRentaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.roses.backend.models.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.roses.backend.models.entities.Variedad;
-
-public interface IVariedadRepository extends JpaRepository<Variedad, Integer> {
-
-}
